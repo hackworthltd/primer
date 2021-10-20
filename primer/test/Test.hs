96ce798eaa99f29f1c794d7ee6754e6c5a648b3b
George Thomas <georgefsthomas@gmail.com>
2021-10-20 14:54:37 +0100

    Update `tasty-discover` comment to point to more relevant part of issue

--- primer/test/Test.hs
+++ primer/test/Test.hs
@@ -1,4 +1,4 @@
-{-TODO We should be able to pass `--modules="Tests/*"` to stop the preprocessor from wasting time searching elsewhere:
-https://github.com/haskell-works/tasty-discover/issues/12
+{-TODO We should be able to pass `-optF --modules=Tests/*` to stop the preprocessor from wasting time searching elsewhere:
+https://github.com/haskell-works/tasty-discover/issues/12#issuecomment-947689298
 -}
 {-# OPTIONS_GHC -F -pgmF tasty-discover -optF --tree-display #-}
