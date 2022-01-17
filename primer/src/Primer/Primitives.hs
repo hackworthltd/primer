7eb768cd385c80598bf3331fa60500f630318021
George Thomas <georgefsthomas@gmail.com>
2022-01-17 11:56:58 +0000

    Formatting pass

--- primer/src/Primer/Primitives.hs
+++ primer/src/Primer/Primitives.hs
@@ -41,58 +41,58 @@ globalPrims =
     [ let name = "toUpper"
        in ( name
           , PrimFun
-              { primFunType = tcon "Char" `tfun` tcon "Char"
-              , primFunDef = \case
-                  [PrimCon _ (PrimChar c)] ->
-                    Right $ char $ toUpper c
-                  xs -> Left $ PrimFunTypeError name xs
-              }
+            { primFunType = tcon "Char" `tfun` tcon "Char"
+            , primFunDef = \case
+                [PrimCon _ (PrimChar c)] ->
+                  Right $ char $ toUpper c
+                xs -> Left $ PrimFunTypeError name xs
+            }
           )
     , let name = "isSpace"
        in ( name
           , PrimFun
-              { primFunType = tcon "Char" `tfun` tcon "Bool"
-              , primFunDef = \case
-                  [PrimCon _ (PrimChar c)] ->
-                    Right $ bool_ $ isSpace c
-                  xs -> Left $ PrimFunTypeError name xs
-              }
+            { primFunType = tcon "Char" `tfun` tcon "Bool"
+            , primFunDef = \case
+                [PrimCon _ (PrimChar c)] ->
+                  Right $ bool_ $ isSpace c
+                xs -> Left $ PrimFunTypeError name xs
+            }
           )
     , let name = "hexToNat"
        in ( name
           , PrimFun
-              { primFunType = tcon "Char" `tfun` (tcon "Maybe" `tapp` tcon "Nat")
-              , primFunDef = \case
-                  [PrimCon _ (PrimChar c)] -> do
-                    Right $ maybe_ (tcon "Nat") nat $ digitToIntSafe c
-                    where
-                      digitToIntSafe :: Char -> Maybe Natural
-                      digitToIntSafe c' = fromIntegral <$> (guard (isHexDigit c') $> digitToInt c')
-                  xs -> Left $ PrimFunTypeError name xs
-              }
+            { primFunType = tcon "Char" `tfun` (tcon "Maybe" `tapp` tcon "Nat")
+            , primFunDef = \case
+                [PrimCon _ (PrimChar c)] -> do
+                  Right $ maybe_ (tcon "Nat") nat $ digitToIntSafe c
+                  where
+                    digitToIntSafe :: Char -> Maybe Natural
+                    digitToIntSafe c' = fromIntegral <$> (guard (isHexDigit c') $> digitToInt c')
+                xs -> Left $ PrimFunTypeError name xs
+            }
           )
     , let name = "natToHex"
        in ( name
           , PrimFun
-              { primFunType = tcon "Nat" `tfun` (tcon "Maybe" `tapp` tcon "Char")
-              , primFunDef = \case
-                  [exprToNat -> Just n] ->
-                    Right $ maybe_ (tcon "Char") char $ intToDigitSafe n
-                    where
-                      intToDigitSafe :: Natural -> Maybe Char
-                      intToDigitSafe n' = guard (0 <= n && n <= 15) $> intToDigit (fromIntegral n')
-                  xs -> Left $ PrimFunTypeError name xs
-              }
+            { primFunType = tcon "Nat" `tfun` (tcon "Maybe" `tapp` tcon "Char")
+            , primFunDef = \case
+                [exprToNat -> Just n] ->
+                  Right $ maybe_ (tcon "Char") char $ intToDigitSafe n
+                  where
+                    intToDigitSafe :: Natural -> Maybe Char
+                    intToDigitSafe n' = guard (0 <= n && n <= 15) $> intToDigit (fromIntegral n')
+                xs -> Left $ PrimFunTypeError name xs
+            }
           )
     , let name = "eqChar"
        in ( name
           , PrimFun
-              { primFunType = tcon "Char" `tfun` (tcon "Char" `tfun` tcon "Bool")
-              , primFunDef = \case
-                  [PrimCon _ (PrimChar c1), PrimCon _ (PrimChar c2)] ->
-                    Right $ bool_ $ c1 == c2
-                  xs -> Left $ PrimFunTypeError name xs
-              }
+            { primFunType = tcon "Char" `tfun` (tcon "Char" `tfun` tcon "Bool")
+            , primFunDef = \case
+                [PrimCon _ (PrimChar c1), PrimCon _ (PrimChar c2)] ->
+                  Right $ bool_ $ c1 == c2
+                xs -> Left $ PrimFunTypeError name xs
+            }
           )
     ]
   where
