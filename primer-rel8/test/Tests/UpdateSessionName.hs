5ce4bcb1ae2a8c0cbf9a96db73f6fe412b6cf914
Drew Hess <src@drewhess.com>
2022-03-02 01:38:57 +0000

    test(primer-rel8): Add tests for `updateSessionName`.
    Note that these tests revealed an issue with the Rel8 implementation,
    namely that if you try to `updateSessionName` using a session ID that
    doesn't exist, the update will not update any rows, but this is not
    detected as an error (as Rel8 will return 0 rows modified).
    
    This should be flagged as an exception, as it means that we've done
    something wrong (we should never try to update a session that doesn't
    already exist in the database). However, I want to wait on
    implementing this until I've had time to create a proper exception
    type for Rel8/Hasql exceptions. Therefore, I've wrapped this test with
    `expectFailBecause` from tasty-expected-failure for now.
    
    See:
    https://github.com/hackworthltd/primer/issues/269

---
+++ primer-rel8/test/Tests/UpdateSessionName.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE BlockArguments #-}
+
+module Tests.UpdateSessionName where
+
+import Foreword
+
+import Primer.App (
+  newEmptyApp,
+ )
+import Primer.Database (
+  SessionData (..),
+  insertSession,
+  newSessionId,
+  querySessionId,
+  safeMkSessionName,
+  updateSessionName,
+ )
+import Primer.Database.Rel8 (
+  runRel8Db,
+ )
+import Test.Tasty (TestTree)
+import Test.Tasty.ExpectedFailure (
+  expectFailBecause,
+ )
+import Test.Tasty.HUnit (testCaseSteps)
+import TestUtils (
+  anyException,
+  assertException,
+  withDbSetup,
+  (@?=),
+ )
+
+test_updateSessionName_roundtrip :: TestTree
+test_updateSessionName_roundtrip = testCaseSteps "updateSessionName database round-tripping" $ \step' ->
+  withDbSetup \conn -> do
+    flip runRel8Db conn $ do
+      let step = liftIO . step'
+
+      step "Insert a new session"
+      let version = "git123"
+      let name = safeMkSessionName "new app"
+      sessionId <- liftIO newSessionId
+      insertSession version sessionId newEmptyApp name
+
+      step "Update it with the same version and name"
+      updateSessionName version sessionId name
+      r1 <- querySessionId version sessionId
+      r1 @?= Right (SessionData newEmptyApp name)
+
+      step "Update it with a new version, but the same name"
+      let newVersion = "new-" <> version
+      updateSessionName newVersion sessionId name
+      r2 <- querySessionId newVersion sessionId
+      r2 @?= Right (SessionData newEmptyApp name)
+
+      step "Update it with a new name"
+      let newName = safeMkSessionName "new new app"
+      updateSessionName newVersion sessionId newName
+      r3 <- querySessionId newVersion sessionId
+      r3 @?= Right (SessionData newEmptyApp newName)
+
+      -- Note: at the moment, we ignore the stored Primer version when
+      -- we query the database. This is a bit odd, but it's not yet
+      -- clear whether it'll be useful to include the version in the
+      -- query.
+      --
+      -- See https://github.com/hackworthltd/primer/issues/268
+      step "We can still query the program using the old version"
+      r4 <- querySessionId version sessionId
+      r4 @?= Right (SessionData newEmptyApp newName)
+
+-- Not yet implemented. See:
+-- https://github.com/hackworthltd/primer/issues/269
+test_updateSessionName_failure :: TestTree
+test_updateSessionName_failure = expectFailBecause "Not yet implemented" $
+  testCaseSteps "updateSessionName failure modes" $ \step' ->
+    withDbSetup \conn -> do
+      flip runRel8Db conn $ do
+        let step = liftIO . step'
+
+        step "Attempt to update a session that hasn't yet been inserted"
+        let version = "git123"
+        let name = safeMkSessionName "this session doesn't exist"
+        sessionId <- liftIO newSessionId
+        assertException "updateSessionName" anyException $ updateSessionName version sessionId name
