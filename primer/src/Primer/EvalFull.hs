773564bef30cbe506f109296b5b57c0000f69379
Ben Price <ben@hackworthltd.com>
2022-06-01 14:14:00 +0100

    docs: comment EvalFull CaseRedex not cause capture

--- primer/src/Primer/EvalFull.hs
+++ primer/src/Primer/EvalFull.hs
@@ -498,6 +498,8 @@ runRedex = \case
     | otherwise -> letType b (pure tyS) $ letType a (pure tyS) (pure t) `ann` pure tyT
   -- case C as : T of ... ; C xs -> e ; ...   ~>  let xs=as:As in e for constructor C of type T, where args have types As
   -- (and also the non-annotated-constructor case)
+  -- Note that when forming the CaseRedex we checked that the variables @xs@ were fresh for @as@ and @As@,
+  -- so this will not capture any variables.
   CaseRedex _ as _ xs e -> foldrM (\(x, (a, tyA)) t -> let_ x (pure a `ann` pure tyA) (pure t)) e (zip xs as)
   -- [ t : T ]  ~>  t  writing [_] for the embedding of syn into chk
   Upsilon e _ -> pure e
