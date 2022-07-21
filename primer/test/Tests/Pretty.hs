f1c7eb706a844cb93b241cdd411c5ee3bd84f7ee
Patrick Aldis <patrick@hackworthltd.com>
2022-07-21 11:26:58 +0100

    feat: Pretty printer testing tree added

--- primer/test/Tests/Pretty.hs
+++ primer/test/Tests/Pretty.hs
@@ -2,25 +2,39 @@ module Tests.Pretty where
 
 import Foreword
 
-import qualified Data.ByteString.Lazy as BS
+import Data.ByteString.Lazy qualified as BS
 import Prettyprinter (defaultLayoutOptions, layoutSmart)
-import Prettyprinter.Render.Terminal (renderStrict)
+import Prettyprinter.Internal.Type (Doc)
+import Prettyprinter.Render.Terminal (AnsiStyle, renderStrict)
 import Primer.Core (ASTDef (..), Def (..), mkSimpleModuleName)
 import Primer.Core.DSL (create')
 import Primer.Examples (comprehensive)
-import Primer.Pretty (defaultPrettyOptions, prettyExpr)
-import Test.Tasty (TestTree)
+import Primer.Pretty (defaultPrettyOptions, prettyExpr, prettyType)
+import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsString)
 
-test_1 :: TestTree
-test_1 = goldenVsString "pretty" "test/outputs/pretty" $ do
+test_comp :: TestTree
+test_comp = testGroup "Comprehensive" [checkExpr, checkType]
+
+checkExpr :: TestTree
+checkExpr = goldenVsString "Expr" "test/outputs/Pretty/Expr" $ do
   case snd $ create' $ comprehensive $ mkSimpleModuleName "Module" of
     DefPrim _ -> exitFailure
-    DefAST e ->
-      pure
-        . BS.fromStrict
-        . encodeUtf8
-        . renderStrict
-        . layoutSmart defaultLayoutOptions
-        . prettyExpr defaultPrettyOptions
-        $ astDefExpr e
+    DefAST e -> docToBS (prettyExpr defaultPrettyOptions $ astDefExpr e)
+
+checkType :: TestTree
+checkType = goldenVsString "Type" "test/outputs/Pretty/Type" $ do
+  case create' $ fmap snd $ comprehensive $ mkSimpleModuleName "Module" of
+    DefPrim _ -> exitFailure
+    DefAST e -> docToBS (prettyType defaultPrettyOptions $ astDefType e)
+
+docToBS :: Doc AnsiStyle -> IO BS.ByteString
+docToBS =
+  pure
+    . BS.fromStrict
+    . encodeUtf8
+    . renderStrict
+    . layoutSmart defaultLayoutOptions
+
+createComp :: Def
+createComp = snd $ create' $ comprehensive $ mkSimpleModuleName "Module"
