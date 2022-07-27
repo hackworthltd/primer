cb780227d88cdba4b0e062730bf9108157e45b11
Patrick Aldis <patrick@hackworthltd.com>
2022-07-27 09:58:42 +0100

    feat: added `compact` and `sparse` option sets

--- primer/test/Tests/Pretty.hs
+++ primer/test/Tests/Pretty.hs
@@ -5,14 +5,14 @@ import Foreword hiding (not)
 import Data.ByteString.Lazy qualified as BS
 import Data.String
 import Data.Text qualified as T
-import Prettyprinter (defaultLayoutOptions, layoutSmart)
+import Prettyprinter (defaultLayoutOptions, layoutSmart, line)
 import Prettyprinter.Internal.Type (Doc)
 import Prettyprinter.Render.Terminal (AnsiStyle, renderStrict)
 import Primer.Core (ASTDef (..), Def (..), GVarName, GlobalName (baseName), ModuleName, mkSimpleModuleName)
 import Primer.Core.DSL (S, create')
 import Primer.Examples (comprehensive, not)
 import Primer.Name (unName)
-import Primer.Pretty (defaultPrettyOptions, prettyExpr, prettyType)
+import Primer.Pretty (PrettyOptions, compact, prettyExpr, prettyType, sparse)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsString)
 
@@ -21,11 +21,23 @@ test_examples = testGroup "Examples" $ map prettyTestGroup [comprehensive, not]
 
 type PrettyTestHandler = (ASTDef -> Doc AnsiStyle, Text)
 
-exprHandler :: PrettyTestHandler
-exprHandler = (prettyExpr defaultPrettyOptions . astDefExpr, "Expr")
+exprHandler :: PrettyOptions -> Text -> PrettyTestHandler
+exprHandler opts name = (prettyExpr opts . astDefExpr, name)
 
-typeHandler :: PrettyTestHandler
-typeHandler = (prettyType defaultPrettyOptions . astDefType, "Type")
+typeHandler :: PrettyOptions -> Text -> PrettyTestHandler
+typeHandler opts name = (prettyType opts . astDefType, name)
+
+exprHandlerSparse :: PrettyTestHandler
+exprHandlerSparse = exprHandler sparse "Expr (Sparse)"
+
+exprHandlerCompact :: PrettyTestHandler
+exprHandlerCompact = exprHandler compact "Expr (Compact)"
+
+typeHandlerSparse :: PrettyTestHandler
+typeHandlerSparse = typeHandler sparse "Type (Sparse)"
+
+typeHandlerCompact :: PrettyTestHandler
+typeHandlerCompact = typeHandler compact "Type (Compact)"
 
 prettyTest :: String -> Def -> PrettyTestHandler -> TestTree
 prettyTest name def handler = goldenVsString hname path bs
@@ -37,7 +49,7 @@ prettyTest name def handler = goldenVsString hname path bs
       DefAST e -> docToBS (fst handler e)
 
 prettyTestGroup :: (ModuleName -> S (GVarName, Def)) -> TestTree
-prettyTestGroup x = testGroup dname (map (prettyTest dname d) [exprHandler, typeHandler])
+prettyTestGroup x = testGroup dname (map (prettyTest dname d) [exprHandlerSparse, exprHandlerCompact, typeHandlerSparse, typeHandlerCompact])
   where
     (n, d) = create' $ x $ mkSimpleModuleName "Module"
     dname = T.unpack . unName . baseName $ n
