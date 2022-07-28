51d35927bdbb878a2013f5fe460e2d415c017c7f
Drew Hess <src@drewhess.com>
2022-07-28 01:43:41 +0100

    feat!: Change 'VJSON' encoding for all-nullary sum types.
    All-nullary sum types now are JSON-encoded as plain strings; i.e.,
    as "EnumValue", rather than "tag": "EnumValue".
    
    BREAKING CHANGE: this breaks the database encoding of programs and
    requires a database migration (though not a schema migration).
    However, as we don't have any programs we currently care about, we do
    not provide a database migration here. Old programs will break.

--- primer/src/Primer/JSON.hs
+++ primer/src/Primer/JSON.hs
@@ -1,23 +1,36 @@
-module Primer.JSON (CustomJSON (..), VJSON, VJSONPrefix, ToJSON, FromJSON, ToJSONKey, FromJSONKey) where
+module Primer.JSON (
+  CustomJSON (..),
+  VJSON,
+  VJSONPrefix,
+  ToJSON,
+  FromJSON,
+  ToJSONKey,
+  FromJSONKey,
+) where
 
 import Foreword
 
 import Data.Aeson (FromJSONKey, ToJSONKey)
 import Data.List (stripPrefix)
-import Deriving.Aeson
+import Deriving.Aeson (
+  CustomJSON (..),
+  FieldLabelModifier,
+  FromJSON,
+  StringModifier (..),
+  ToJSON,
+ )
+import Deriving.Aeson.Stock (
+  Vanilla,
+ )
 
 -- | A type for 'Primer' style JSON encoding.
--- All our types use this, so we generate consistent encodings for the whole
--- API.
--- This is designed to be compatible with purescript-foreign-generic.
-type VJSON a = CustomJSON '[NoAllNullaryToStringTag] a
+type VJSON a = Vanilla a
 
--- | Like 'VJSON' but strips the given prefix from field names.
--- Useful if your Haskell fields are fooA, fooB and you want the corresponding purescript
--- record to use a, b.
--- This should only be used for records whose fields all begin with the given prefix, followed by a
--- capital letter.
-type VJSONPrefix prefix a = CustomJSON '[NoAllNullaryToStringTag, FieldLabelModifier (StripPrefixAndStartLowercase prefix)] a
+-- | Like 'VJSON', but strips the given prefix from field names.
+--
+-- This type should only be used for records whose fields all begin with
+-- the given prefix, followed by a capital letter.
+type VJSONPrefix prefix a = CustomJSON '[FieldLabelModifier (StripPrefixAndStartLowercase prefix)] a
 
 data StripPrefixAndStartLowercase prefix
 
