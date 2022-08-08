46bd02367d3ffea98bd094f846a394814037931b
Patrick Aldis <patrick@hackworthltd.com>
2022-08-08 12:01:33 +0100

    fix: `prelude` exports all `logic` functions
    Now exports `xor` and `implies`

--- primer/src/Primer/Prelude.hs
+++ primer/src/Primer/Prelude.hs
@@ -5,10 +5,10 @@ import Data.Map qualified as Map
 import Foreword
 import Primer.Core (ID)
 import Primer.Module (Module (Module, moduleDefs, moduleName, moduleTypes))
-import Primer.Prelude.Logic (andDef, notDef, orDef)
+import Primer.Prelude.Logic (andDef, impliesDef, notDef, orDef, xorDef)
 import Primer.Prelude.Utils (modName)
 
 prelude :: (MonadFresh ID m) => m Module
 prelude = do
-  defs <- traverse sequence [("not", notDef), ("and", andDef), ("or", orDef)]
+  defs <- traverse sequence [("not", notDef), ("and", andDef), ("or", orDef), ("xor", xorDef), ("implies", impliesDef)]
   pure Module{moduleName = modName, moduleTypes = Map.empty, moduleDefs = Map.fromList defs}
