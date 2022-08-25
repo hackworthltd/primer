b5fa820b01ea02547ce5b3b51f6f08cb9418920c
Patrick Aldis <patrick@hackworthltd.com>
2022-08-25 15:43:17 +0100

    refactor: rewrite `functionOutput` in terms of `functionOutput'`
    note: preparation for polymorphism testing

--- primer/test/Tests/Prelude/Utils.hs
+++ primer/test/Tests/Prelude/Utils.hs
@@ -3,8 +3,8 @@ module Tests.Prelude.Utils where
 import Foreword
 import Hedgehog (MonadTest, (===))
 import Optics (over)
-import Primer.Core (Expr, GVarName)
-import Primer.Core.DSL (apps, create', gvar)
+import Primer.Core (Expr, GVarName, Type)
+import Primer.Core.DSL (apps', create', gvar)
 import Primer.EvalFull (Dir (Chk), EvalFullError, TerminationBound, evalFull)
 import Primer.Module (builtinModule, moduleDefsQualified, moduleTypesQualified, primitiveModule)
 import Primer.Prelude (prelude)
@@ -15,11 +15,15 @@ import Tests.EvalFull (evalResultExpr)
 (<===>) :: (HasCallStack, MonadTest m) => Either EvalFullError Expr -> Either EvalFullError Expr -> m ()
 x <===> y = withFrozenCallStack $ on (===) (over evalResultExpr zeroIDs) x y
 
--- Tests a prelude function
+-- Tests a prelude function with Expr arguments
 functionOutput :: GVarName -> [TestM Expr] -> TerminationBound -> Either EvalFullError Expr
-functionOutput f args depth =
+functionOutput f args = functionOutput' f (map Left args)
+
+-- Tests a prelude function with a combination of Expr/Type arguments to be applied
+functionOutput' :: GVarName -> [Either (TestM Expr) (TestM Type)] -> TerminationBound -> Either EvalFullError Expr
+functionOutput' f args depth =
   evalTestM 0 $ do
-    e <- apps (gvar f) args
+    e <- apps' (gvar f) args
     evalFull ty def n d e
   where
     mods = [builtinModule, primitiveModule, prelude']
