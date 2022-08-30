ddd8050b06f6a3c1f607af08257e07442e369e8b
Ben Price <ben@hackworthltd.com>
2022-08-30 13:48:38 +0100

    test: check examples are well-typed, except comprehensive
    We also check that comprehensive is ill-typed.

---
+++ primer/test/Tests/Examples.hs
@@ -0,0 +1,61 @@
+module Tests.Examples where
+
+import Foreword hiding (not)
+
+import Primer.Core.DSL (create')
+
+import Data.Map qualified as Map
+import Primer.Core (GlobalName (baseName), ModuleName (ModuleName))
+import Primer.Examples (
+  comprehensive,
+  evenOddModule,
+  mapModule,
+  not,
+ )
+import Primer.Module (Module (Module), builtinModule)
+import Primer.Typecheck (
+  CheckEverythingRequest (CheckEverything, toCheck, trusted),
+  SmartHoles (NoSmartHoles),
+  checkEverything,
+ )
+import Test.Tasty.HUnit (Assertion, assertFailure)
+import Tests.Typecheck (runTypecheckTestM)
+
+checkExamplesRequest :: CheckEverythingRequest
+checkExamplesRequest =
+  CheckEverything
+    { trusted = [builtinModule]
+    , toCheck =
+        [ fst $ mapModule $ ModuleName $ pure "MapModule"
+        , fst $ evenOddModule $ ModuleName $ pure "EvenOddModule"
+        , otherExamples
+        ]
+    }
+  where
+    others = ModuleName $ pure "OtherExamples"
+    otherExamples =
+      Module others mempty $
+        Map.fromList
+          [ first baseName $ create' $ not others
+          ]
+
+unit_check_examples :: Assertion
+unit_check_examples = case runTypecheckTestM
+  NoSmartHoles
+  (checkEverything NoSmartHoles checkExamplesRequest) of
+  Left err -> assertFailure $ show err
+  Right _ -> pure ()
+
+unit_comprehensive_ill_typed :: Assertion
+unit_comprehensive_ill_typed = case runTypecheckTestM
+  NoSmartHoles
+  ( checkEverything NoSmartHoles $
+      CheckEverything
+        { trusted = [builtinModule]
+        , toCheck = [Module modName mempty $ Map.fromList [first baseName $ create' $ comprehensive modName]]
+        }
+  ) of
+  Left _ -> pure ()
+  Right _ -> assertFailure "expected comprehensive to be ill-typed"
+  where
+    modName = ModuleName $ pure "Comprehensive"
