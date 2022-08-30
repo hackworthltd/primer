f343795bafab595ab6470cb500e77b7d23d0f4e1
Patrick Aldis <patrick@hackworthltd.com>
2022-08-30 10:15:34 +0100

    fix: `id`, `const` test multiple types

--- primer/test/Tests/Prelude/Polymorphism.hs
+++ primer/test/Tests/Prelude/Polymorphism.hs
@@ -3,11 +3,11 @@ module Tests.Prelude.Polymorphism where
 import Foreword
 
 import Hedgehog (forAll)
-import Hedgehog.Gen (integral_)
+import Hedgehog.Gen qualified as G
 import Hedgehog.Range qualified as Range
-import Primer.Builtins (tBool)
-import Primer.Builtins.DSL (bool_)
-import Primer.Core.DSL (create', int, tcon)
+import Primer.Builtins (tBool, tList)
+import Primer.Builtins.DSL (bool_, list_)
+import Primer.Core.DSL (create', int, tapp, tcon)
 import Primer.Prelude.Polymorphism qualified as P
 import Primer.Primitives (tInt)
 import Tasty (Property, property)
@@ -15,13 +15,19 @@ import Tests.Prelude.Utils (functionOutput', (<===>))
 
 tasty_id_prop :: Property
 tasty_id_prop = property $ do
-  n <- forAll $ integral_ (Range.constant (-10) 10)
-  functionOutput' P.id [Right $ tcon tInt, Left $ int n] 20 <===> Right (create' $ int n)
+  n <- forAll $ G.integral_ (Range.constant (-10) 10)
+  b <- forAll G.bool_
+  ns <- forAll $ G.list (Range.constant 0 10) (G.integral_ (Range.constant (-10) 10))
+  functionOutput' P.id [Right $ tcon tInt, Left $ int n] 20 <===> Right (create' $ int n) -- Integer Test
+  functionOutput' P.id [Right $ tcon tBool, Left $ bool_ b] 20 <===> Right (create' $ bool_ b) -- Bool Test
+  functionOutput' P.id [Right $ tcon tList `tapp` tcon tInt, Left $ list_ tInt $ map int ns] 20 <===> Right (create' $ list_ tInt $ map int ns) -- List of Int Test
 
 tasty_const_prop :: Property
 tasty_const_prop = property $ do
-  n <- forAll $ integral_ (Range.constant (-10) 10)
-  functionOutput'
+  n <- forAll $ G.integral_ (Range.constant (-10) 10)
+  b <- forAll G.bool_
+  ns <- forAll $ G.list (Range.constant 0 10) (G.integral_ (Range.constant (-10) 10))
+  functionOutput' -- Integer Test
     P.const
     [ Right $ tcon tInt
     , Left $ int n
@@ -30,3 +36,21 @@ tasty_const_prop = property $ do
     ]
     20
     <===> Right (create' $ int n)
+  functionOutput' -- Bool Test
+    P.const
+    [ Right $ tcon tBool
+    , Left $ bool_ b
+    , Right $ tcon tInt
+    , Left $ int n
+    ]
+    20
+    <===> Right (create' $ bool_ b)
+  functionOutput' -- List of Int Test
+    P.const
+    [ Right $ tcon tList `tapp` tcon tInt
+    , Left $ list_ tInt $ map int ns
+    , Right $ tcon tInt
+    , Left $ int n
+    ]
+    20
+    <===> Right (create' $ list_ tInt $ map int ns)
