948af8ada14a52c46af1c32112700c47c13eb4d2
Drew Hess <src@drewhess.com>
2022-09-04 18:12:08 +0100

    test: Add tests to ensure database queue is empty when no exception.

--- primer/test/Tests/Database.hs
+++ primer/test/Tests/Database.hs
@@ -2,13 +2,45 @@ module Tests.Database where
 
 import Foreword
 
+import Control.Concurrent.STM (
+  atomically,
+  isEmptyTBQueue,
+  newEmptyTMVarIO,
+  newTBQueueIO,
+  writeTBQueue,
+ )
 import Data.Text qualified as Text
+import Primer.API (
+  Env (..),
+  PrimerIO,
+  addSession,
+  edit,
+  listSessions,
+  renameSession,
+  runPrimerIO,
+ )
+import Primer.App (
+  MutationRequest (Edit),
+  ProgAction (CreateDef),
+ )
+import Primer.Core (
+  mkSimpleModuleName,
+ )
 import Primer.Database (
+  OffsetLimit (OL),
+  Op (LoadSession),
+  ServiceCfg (..),
   defaultSessionName,
   fromSessionName,
   mkSessionName,
+  runNullDb,
   safeMkSessionName,
+  serve,
  )
+import Primer.Examples (
+  even3App,
+ )
+import StmContainers.Map qualified as StmMap
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -95,6 +127,34 @@ test_invalid =
             safeMkSessionName t @?= defaultSessionName
         ]
 
+test_insert_empty_q :: TestTree
+test_insert_empty_q = empty_q_harness "database Insert leaves an empty op queue" $ do
+  void $ addSession "even3App" even3App
+
+test_updateapp_empty_q :: TestTree
+test_updateapp_empty_q = empty_q_harness "database UpdateApp leaves an empty op queue" $ do
+  sid <- addSession "even3App" even3App
+  void $ edit sid $ Edit [CreateDef (mkSimpleModuleName "Even3") $ Just "newDef"]
+
+test_updatename_empty_q :: TestTree
+test_updatename_empty_q = empty_q_harness "database UpdateName leaves an empty op queue" $ do
+  sid <- addSession "even3App" even3App
+  void $ renameSession sid "even3App'"
+
+test_loadsession_empty_q :: TestTree
+test_loadsession_empty_q = empty_q_harness "database LoadSession leaves an empty op queue" $ do
+  sid <- addSession "even3App" even3App
+  -- No easy way to do this from the API, so we do it here by hand.
+  callback <- liftIO newEmptyTMVarIO
+  q <- asks dbOpQueue
+  ss <- asks sessions
+  void $ liftIO $ atomically $ writeTBQueue q $ LoadSession sid ss callback
+
+test_listsessions_empty_q :: TestTree
+test_listsessions_empty_q = empty_q_harness "database ListSessions leaves an empty op queue" $ do
+  void $ addSession "even3App" even3App
+  void $ listSessions True $ OL 0 $ Just 100
+
 testSessionName :: TestName -> Text -> Text -> TestTree
 testSessionName testName t expected =
   testGroup
@@ -105,3 +165,28 @@ testSessionName testName t expected =
     , testCase "safe" $
         fromSessionName (safeMkSessionName t) @?= expected
     ]
+
+empty_q_harness :: Text -> PrimerIO () -> TestTree
+empty_q_harness desc test = testCaseSteps (toS desc) $ \step' -> do
+  dbOpQueue <- newTBQueueIO 4
+  inMemorySessions <- StmMap.newIO
+  dbSessions <- StmMap.newIO
+  let version = "git123"
+  nullDbProc <- async $ runNullDb dbSessions $ serve $ ServiceCfg dbOpQueue version
+  testProc <- async $ flip runPrimerIO (Env inMemorySessions dbOpQueue version) $ do
+    test
+    -- Give 'nullDbProc' time to empty the queue.
+    liftIO $ threadDelay 100000
+  (p, result) <- waitAnyCatchCancel [nullDbProc, testProc]
+  case result of
+    Right _ ->
+      if p == testProc
+        then do
+          step' "Check that the database op queue is empty"
+          qempty <- liftIO $ atomically $ isEmptyTBQueue dbOpQueue
+          assertBool "Queue should be empty" qempty
+        else assertFailure "the impossible happened: nullDbProc exited"
+    Left e ->
+      if p == testProc
+        then assertFailure $ "nullDbProc threw an exception: " <> show e
+        else assertFailure $ "testProc threw an exception: " <> show e
