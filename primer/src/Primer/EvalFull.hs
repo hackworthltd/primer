3bcd4aab5d6565adc5848e49b8fb084d936a6431
Ben Price <ben@hackworthltd.com>
2022-10-04 00:55:43 +0100

    more debugging

--- primer/src/Primer/EvalFull.hs
+++ primer/src/Primer/EvalFull.hs
@@ -435,9 +435,13 @@ viewRedex :: TypeDefMap ->
   Dir ->
   Expr -> Reader Cxt (Maybe Redex)
 viewRedex tydefs globals dir = \case
+  e | trace @Text ("viewRedex " <> show e) False -> undefined
   Var _ (GlobalVarRef x) | Just (DefAST y) <- x `M.lookup` globals -> purer $ InlineGlobal x y
   Var _ (LocalVarRef v) -> do
-    getNonCapturedLocal v <&> \x -> do
+    x <- getNonCapturedLocal v
+    trace @Text ("viewRedex, gncl: " <> show x) $
+     pure $
+--    getNonCapturedLocal v <&> \x -> do
       case x of
         Just (LSome (LLet _ e)) -> pure $ InlineLet v e
         Just (LSome (LLetrec _ e t)) -> pure $ InlineLetrec v e t
