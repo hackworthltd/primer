444633b811d726a8f8a5a42585ef979c2670a088
github-action-benchmark <github@users.noreply.github.com>
2022-11-20 19:37:54 +0000

    add Primer benchmarks (customSmallerIsBetter) benchmark result for 402edf771f3a2243b16e4ac5ef76e3e2ce1ee2a6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668972250469,
+  "lastUpdate": 1668973074079,
   "repoUrl": "https://github.com/hackworthltd/primer",
   "entries": {
     "Primer benchmarks": [
@@ -23,6 +23,170 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/hackworthltd/primer/commit/27a82efeefcbcb70c49cbb91af48f3f7697bf4dc"
         },
         "date": 1668972249855,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "evalTestM/pure logs/mapEven 1",
+            "value": 0.0072215021636359246,
+            "unit": "mean time",
+            "range": 0.0003326512156061708
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 1",
+            "value": 0.2175795295835449,
+            "unit": "outlier variance"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 1",
+            "value": 0.007124531281730202,
+            "unit": "time",
+            "extra": "R²: 0.9946078376958374"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 1",
+            "value": 21688990.444938242,
+            "unit": "allocated",
+            "extra": "R²: 0.9999999999591914"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 1",
+            "value": 0.014073561376026611,
+            "unit": "peakMbAllocated",
+            "extra": "R²: 0.26308480056154604"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 1",
+            "value": 5.204230568406734,
+            "unit": "numGcs",
+            "extra": "R²: 0.9999811494190667"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 10",
+            "value": 0.22358061694378395,
+            "unit": "mean time",
+            "range": 0.006542774646985675
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 10",
+            "value": 0.13888888888888867,
+            "unit": "outlier variance"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 10",
+            "value": 0.22425415756962527,
+            "unit": "time",
+            "extra": "R²: 0.9973009573747145"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 10",
+            "value": 744134501.7142857,
+            "unit": "allocated",
+            "extra": "R²: 0.9999999999987537"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 10",
+            "value": 0.37142857142857405,
+            "unit": "peakMbAllocated",
+            "extra": "R²: 0.8521008403361344"
+          },
+          {
+            "name": "evalTestM/pure logs/mapEven 10",
+            "value": 178.62857142857146,
+            "unit": "numGcs",
+            "extra": "R²: 0.9999992495475966"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 1",
+            "value": 0.006220037377684667,
+            "unit": "mean time",
+            "range": 0.00023810843879496564
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 1",
+            "value": 0.1788747089585761,
+            "unit": "outlier variance"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 1",
+            "value": 0.0064460652753486085,
+            "unit": "time",
+            "extra": "R²: 0.992767019893218"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 1",
+            "value": 19325748.197285306,
+            "unit": "allocated",
+            "extra": "R²: 0.9999999999629059"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 1",
+            "value": -7.742222811169396e-16,
+            "unit": "peakMbAllocated",
+            "extra": "R²: -inf"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 1",
+            "value": 4.636934999776014,
+            "unit": "numGcs",
+            "extra": "R²: 0.9999731926298188"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 10",
+            "value": 0.19698637085594883,
+            "unit": "mean time",
+            "range": 0.02167317826189191
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 10",
+            "value": 0.3094302779201393,
+            "unit": "outlier variance"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 10",
+            "value": 0.179320440200224,
+            "unit": "time",
+            "extra": "R²: 0.9853290362316423"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 10",
+            "value": 646780790.8571429,
+            "unit": "allocated",
+            "extra": "R²: 0.9999999999977748"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 10",
+            "value": 0,
+            "unit": "peakMbAllocated",
+            "extra": "R²: null"
+          },
+          {
+            "name": "evalTestM/discard logs/mapEven 10",
+            "value": 155.22857142857146,
+            "unit": "numGcs",
+            "extra": "R²: 0.9999990062398538"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "src@drewhess.com",
+            "name": "Drew Hess",
+            "username": "dhess"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "402edf771f3a2243b16e4ac5ef76e3e2ce1ee2a6",
+          "message": "Merge pull request #787 from hackworthltd/dhess/fix-check-hls\n\nfix: Only run `check-hls` when Haskell source files have changed.",
+          "timestamp": "2022-11-20T19:31:11Z",
+          "tree_id": "beb0727326c78f16ec667a97e9a4fe23599798aa",
+          "url": "https://github.com/hackworthltd/primer/commit/402edf771f3a2243b16e4ac5ef76e3e2ce1ee2a6"
+        },
+        "date": 1668973072863,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
