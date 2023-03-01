61c261004283b3fa3dff70499733839d3c44d34c
Ben Price <ben@hackworthltd.com>
2023-03-01 19:17:37 +0000

    change Expected.mapEven to map over a unsat-list of sat-nat

--- primer/src/Primer/Core/DSL.hs
+++ primer/src/Primer/Core/DSL.hs
@@ -8,6 +8,8 @@ module Primer.Core.DSL (
   aPP,
   conSat,
   con,
+  con0,
+  con1,
   lvar,
   gvar,
   var,
@@ -118,6 +120,20 @@ con c = Con <$> meta <*> pure c <*> pure [] <*> pure []
 -- unsaturated constructors)
 conSat :: MonadFresh ID m => ValConName -> [m Type] -> [m Expr] -> m Expr
 conSat c tys tms = Con <$> meta <*> pure c <*> sequence tys <*> sequence tms
+
+-- | Create a constructor of arity zero.
+-- (This condition is not checked here.
+--  If used with a constructor which has fields,
+--  then the typechecker will complain, when run.)
+con0 :: MonadFresh ID m => ValConName -> m Expr
+con0 c = conSat c [] []
+
+-- | Create a constructor of arity one.
+-- (This condition is not checked here.
+--  If used with a constructor which has fields,
+--  then the typechecker will complain, when run.)
+con1 :: MonadFresh ID m => ValConName -> m Expr -> m Expr
+con1 c t = conSat c [] [t]
 
 lvar :: MonadFresh ID m => LVarName -> m Expr
 lvar v = Var <$> meta <*> pure (LocalVarRef v)
