500631476a1c0c177e503b6c21ba03564514b117
Ben Price <ben@hackworthltd.com>
2023-03-09 13:49:07 +0000

    nat: use conSat

--- primer/src/Primer/Builtins/DSL.hs
+++ primer/src/Primer/Builtins/DSL.hs
@@ -45,7 +45,7 @@ bool_ b = con $ if b then cTrue else cFalse
 nat :: MonadFresh ID m => Natural -> m Expr
 nat = \case
   0 -> con cZero
-  n -> app (con cSucc) $ nat (n - 1)
+  n -> conSat cSucc [] [nat (n - 1)]
 
 maybe_ :: MonadFresh ID m => m Type -> (a -> m Expr) -> Maybe a -> m Expr
 maybe_ t f = \case
