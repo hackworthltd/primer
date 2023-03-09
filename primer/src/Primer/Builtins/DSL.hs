c5a04f39f6e56e51114144e2baf8287dc9dea88f
Ben Price <ben@hackworthltd.com>
2023-03-09 13:49:07 +0000

    feat: Builtins.DSL.nat creates saturated constructors
    This is a step towards all constructors being saturated, at which
    point we can update the typechecker to enforce that invariant.

--- primer/src/Primer/Builtins/DSL.hs
+++ primer/src/Primer/Builtins/DSL.hs
@@ -32,6 +32,8 @@ import Primer.Core.DSL (
   aPP,
   app,
   con,
+  con0,
+  con1,
   tapp,
   tcon,
  )
@@ -42,8 +44,8 @@ bool_ b = con $ if b then cTrue else cFalse
 
 nat :: MonadFresh ID m => Natural -> m Expr
 nat = \case
-  0 -> con cZero
-  n -> app (con cSucc) $ nat (n - 1)
+  0 -> con0 cZero
+  n -> con1 cSucc $ nat (n - 1)
 
 maybe_ :: MonadFresh ID m => m Type -> (a -> m Expr) -> Maybe a -> m Expr
 maybe_ t f = \case
