889b024a3f8fba7fc076b6ae7b00fd318236d960
Ben Price <ben@hackworthltd.com>
2023-03-21 20:14:29 +0000

    feat: actions to enter/exit constructor arguments
    Now that constructors directly store their arguments, we need actions
    to enter (and exit) these tree positions, so one can edit them.
    
    It would be possible to combine `Child1`, `Child2` and `ConChild`, but
    for the sake of keeping this commit small and non-breaking we have added
    a new type of movement.

--- primer/src/Primer/Action/Movement.hs
+++ primer/src/Primer/Action/Movement.hs
@@ -6,7 +6,19 @@ import Primer.Core.Meta (ValConName)
 import Primer.JSON (CustomJSON (..), FromJSON, PrimerJSON, ToJSON)
 
 -- | Core movements
-data Movement = Child1 | Child2 | Parent | Branch ValConName
+data Movement
+  = Child1
+  | Child2
+  | Parent
+  | Branch ValConName
+  | -- | Move into a term argument of a saturated constructor (zero-indexed)
+    -- TODO (saturated constructors) the current, temporary, situation is that
+    -- constructors have both type arguments (indices) and term arguments. To move
+    -- into a term argument, one uses this @Movement@ type. To move into a *type*
+    -- argument, use the special 'EnterConTypeArgument' action (it cannot be a
+    -- movement because it changes from focusing on an expression to focusing on a
+    -- type).
+    ConChild Int
   deriving stock (Eq, Show, Read, Generic)
   deriving (FromJSON, ToJSON) via PrimerJSON Movement
   deriving anyclass (NFData)
