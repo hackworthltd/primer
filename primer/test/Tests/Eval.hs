9dc242d913c9ac96d749d8e2ed938838927b28cb
Ben Price <ben@hackworthltd.com>
2023-03-23 18:15:33 +0000

    change primitive functions to return annotated cons

--- primer/test/Tests/Eval.hs
+++ primer/test/Tests/Eval.hs
@@ -543,6 +543,7 @@ unit_tryReduce_prim = do
             `app` char 'a'
             `app` char 'a'
             <*> con0 cTrue
+            `ann` tcon tBool
   result <- runTryReduce tydefs primDefs mempty (expr, i)
   case result of
     Right (expr', ApplyPrimFun detail) -> do
