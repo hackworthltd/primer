d8a9ab9698214bb60438a462a6f1f85509b1f950
Ben Price <ben@hackworthltd.com>
2023-03-28 18:01:40 +0100

    remove unit_type_preservation_case_hole_regression as starting point is impossible (relies on ctor having a hole in its index)

--- primer/test/Tests/EvalFull.hs
+++ primer/test/Tests/EvalFull.hs
@@ -560,33 +560,6 @@ unit_type_preservation_case_regression_ty =
         s2 <- evalFullTest maxID builtinTypes mempty 2 Chk expr
         s2 <~==> Left (TimedOut expected2)
 
--- Consider
---   case Just @? False : Maybe Nat of Just x -> Succ x ; Nothing -> ?
--- In the past we would reduce this to
---   let x = False : Nat in Succ x
--- which is ill-typed (we ignored the hole in the type-application,
--- which acts as a type-changing cast).
--- We simply test that the one-step reduction of this expression is well-typed,
--- without mandating what the result should be.
-unit_type_preservation_case_hole_regression :: Assertion
-unit_type_preservation_case_hole_regression = evalTestM 0 $ do
-  t <-
-    case_
-      ((con cJust [tEmptyHole][con0 cFalse]) `ann` (tcon tMaybe `tapp` tcon tNat))
-      [ branch cNothing [] emptyHole
-      , branch cJust [("x", Nothing)] $ con cSucc [][ lvar "x"]
-      ]
-  let tds = foldMap' moduleTypesQualified testModules
-  let globs = foldMap' moduleDefsQualified testModules
-  ((_steps, s), logs) <- runPureLogT $ evalFullStepCount tds globs 1 Syn t
-  let s' = case s of
-        Left (TimedOut e) -> e
-        Right e -> e
-  pure $ do
-    expectTypedWithPrims $ pure t `ann` tEmptyHole
-    assertNoSevereLogs @EvalLog logs
-    expectTypedWithPrims $ pure s' `ann` tEmptyHole
-
 -- Previously EvalFull reducing a BETA expression could result in variable
 -- capture. We would reduce (Λa.t : ∀b.T) S to
 -- let b = S in (let a = S in t) : T
