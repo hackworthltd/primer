8320b7772f5df69f28c047ec8fed9f1668f5c6cd
Ben Price <ben@hackworthltd.com>
2023-03-28 19:23:24 +0100

    TODO: update comments around case redex

--- primer/src/Primer/Eval/Redex.hs
+++ primer/src/Primer/Eval/Redex.hs
@@ -303,6 +303,7 @@ data Redex
     -- reduction steps. E.g.
     --     cons ==  (Λa λx λxs. Cons @a x xs) : ∀a. a -> List a -> List a
     -- )
+    -- TODO (saturated constructors) update the above comment! NB: only annotated ctors are well-typed
     CaseRedex
       { con :: ValConName
       -- ^ The head of the scrutinee
