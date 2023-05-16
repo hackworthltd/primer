ead8c8945ac467f16bc9a7e82bb2aed8cd1a1fb0
George Thomas <georgefsthomas@gmail.com>
2023-05-17 00:21:21 +0100

    `AddConField` - update TC
    Signed-off-by: George Thomas <georgefsthomas@gmail.com>

--- primer/src/Primer/App.hs
+++ primer/src/Primer/App.hs
@@ -138,6 +138,8 @@ import Primer.Core (
   TyConName,
   Type,
   Type' (..),
+  TypeCache (..),
+  TypeCacheBoth (..),
   TypeMeta,
   ValConName,
   caseBranchName,
@@ -819,14 +821,16 @@ applyProgAction prog = \case
       updateDefs = traverseOf (traversed % #_DefAST % #astDefExpr) (updateCons <=< updateDecons)
       updateCons = transformM $ \case
         Con m con' tms | con' == con -> do
-          m' <- DSL.meta
+          id <- fresh
+          let m' = Meta id (Just (TCEmb $ TCBoth (TEmptyHole ()) (TEmptyHole ()))) Nothing
           case insertAt index (EmptyHole m') tms of
             Just args' -> pure $ Con m con' args'
             Nothing -> throwError $ ConNotSaturated con
         e -> pure e
       updateDecons = transformNamedCaseBranch prog type_ con $
         \(CaseBranch vc binds e) -> do
-          m' <- DSL.meta
+          id <- fresh
+          let m' = Meta id (Just (TCChkedAt (TEmptyHole ()))) Nothing
           newName <- LocalName <$> freshName (freeVars e)
           binds' <- maybe (throwError $ IndexOutOfRange index) pure $ insertAt index (Bind m' newName) binds
           pure $ CaseBranch vc binds' e
