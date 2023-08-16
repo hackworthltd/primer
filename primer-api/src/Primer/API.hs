d131378548a58359344573116d2c1dcfd757728e
Ben Price <ben@hackworthltd.com>
2023-08-16 17:37:14 +0100

    feat!: change primer-api to emit kinds-in-foralls as trees
    Now that we have unique IDs on kinds, this is possible whilst giving
    unique `nodeId`s in the api's result.
    
    BREAKING CHANGE: this affects our API. Our advertised OpenAPI interface
    remains the same, but the data we return will differ.
    
    Signed-off-by: Ben Price <ben@hackworthltd.com>

--- primer-api/src/Primer/API.hs
+++ primer-api/src/Primer/API.hs
@@ -178,7 +178,6 @@ import Primer.Core (
   ValConName,
   getID,
   unLocalName,
-  unsafeMkLocalName,
   _bindMeta,
   _exprMetaLens,
   _kindMeta,
@@ -1014,17 +1013,10 @@ viewTreeType' t0 = case t0 of
   TForall _ n k t ->
     Tree
       { nodeId
-      , body = TextBody $ RecordPair Flavor.TForall $ localName $ unsafeMkLocalName $ withKindAnn $ Name.unName $ unLocalName n
-      , childTrees = [viewTreeType' t]
+      , body = TextBody $ RecordPair Flavor.TForall $ localName n
+      , childTrees = [viewTreeKind' k, viewTreeType' t]
       , rightChild = Nothing
       }
-    where
-      -- TODO this is a placeholder
-      -- for now we expect all kinds in student programs to be `KType`
-      -- but we show something for other kinds, in order to keep rendering injective
-      withKindAnn = case k of
-        KType _ -> identity
-        _ -> (<> (" :: " <> show k))
   TLet _ n t b ->
     Tree
       { nodeId
