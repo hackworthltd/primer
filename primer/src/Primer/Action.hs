07c9ffc4170db541afa4802204e9a157fdc5481f
Ben Price <ben@hackworthltd.com>
2023-09-05 18:01:40 +0100

    feat: offer & accept kind actions in foralls
    This does not change our API, we simply accept some actions in positions
    that we previously did not.
    
    Signed-off-by: Ben Price <ben@hackworthltd.com>

--- primer/src/Primer/Action.hs
+++ primer/src/Primer/Action.hs
@@ -64,6 +64,7 @@ import Primer.Core (
   HasID,
   HasMetadata (_metadata),
   ID,
+  Kind' (KHole),
   KindMeta,
   LVarName,
   LocalName (LocalName, unLocalName),
@@ -101,6 +102,7 @@ import Primer.Core.DSL (
   con,
   emptyHole,
   hole,
+  kfun,
   khole,
   ktype,
   lAM,
@@ -482,14 +484,17 @@ applyAction' a = case a of
   RenameCaseBinding x -> \case
     InBind (BindCase z) -> InBind . BindCase <$> renameCaseBinding x z
     _ -> throwError $ CustomFailure a "cannot rename this node - not a case binding"
-  ConstructKType -> const $ throwError $ CustomFailure ConstructKType "kind edits currently only allowed in typedefs"
-  ConstructKFun -> const $ throwError $ CustomFailure ConstructKFun "kind edits currently only allowed in typedefs"
+  ConstructKType -> kindAction constructKType "cannot construct the kind 'Type' - not in kind"
+  ConstructKFun -> kindAction constructKFun "cannot construct the arrow kind - not in kind"
   where
     termAction f s = \case
       InExpr ze -> InExpr <$> f ze
       _ -> throwError $ CustomFailure a s
     typeAction f s = \case
       InType zt -> InType <$> f zt
+      _ -> throwError $ CustomFailure a s
+    kindAction f s = \case
+      InKind zt -> InKind <$> f zt
       _ -> throwError $ CustomFailure a s
 
 setCursor :: MonadError ActionError m => ID -> ExprZ -> m Loc
@@ -1070,6 +1075,14 @@ renameForall b zt = case target zt of
             throwError NameCapture
   _ ->
     throwError $ CustomFailure (RenameForall b) "the focused expression is not a forall type"
+
+constructKType :: (MonadFresh ID m, MonadError ActionError m) => KindZ -> m KindZ
+constructKType zk = case target zk of
+  KHole _ -> flip replace zk <$> ktype
+  _ -> throwError $ CustomFailure ConstructKType "can only construct the kind 'Type' in hole"
+
+constructKFun :: MonadFresh ID m => KindZ -> m KindZ
+constructKFun zk = flip replace zk <$> ktype `kfun` pure (target zk)
 
 -- | Convert a high-level 'Available.NoInputAction' to a concrete sequence of 'ProgAction's.
 toProgActionNoInput ::
