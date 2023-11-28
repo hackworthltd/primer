89490af44b3a7eebe92797f56e5c1394895db615
Ben Price <ben@hackworthltd.com>
2023-11-28 15:01:15 +0000

    iteratively unroll letrec, prob slower but better than ooming
    With the change
    diff --git a/primer/testlib/Primer/Test/Expected.hs b/primer/testlib/Primer/Test/Expected.hs
    index 21d66f13d..6e813f3dd 100644
    --- a/primer/testlib/Primer/Test/Expected.hs
    +++ b/primer/testlib/Primer/Test/Expected.hs
    @@ -56,7 +56,7 @@ mapEven :: Int -> Expected
     mapEven n =
       let modName = mkSimpleModuleName "TestModule"
           ((globals, e, expected), id) = create $ do
    -        (mapName, mapDef) <- Examples.map modName
    +        (mapName, mapDef) <- Examples.map' modName
             (evenName, evenDef) <- Examples.even modName
             (oddName, oddDef) <- Examples.odd modName
             let lst = list_ $ take n $ iterate (con1 cSucc) (con0 cZero)
    
    we have the following benchmark timings, showing some slowdown
    
    before
    benchmarking evalTestM/interp (has no logs)/mapEven 1
    time                 1.207 ms   (1.194 ms .. 1.227 ms)
                         0.998 R²   (0.996 R² .. 1.000 R²)
    mean                 1.205 ms   (1.197 ms .. 1.217 ms)
    std dev              31.59 μs   (22.76 μs .. 50.10 μs)
    variance introduced by outliers: 14% (moderately inflated)
    
    benchmarking evalTestM/interp (has no logs)/mapEven 10
    time                 5.443 ms   (5.362 ms .. 5.525 ms)
                         0.996 R²   (0.992 R² .. 0.998 R²)
    mean                 5.502 ms   (5.435 ms .. 5.605 ms)
    std dev              263.5 μs   (203.3 μs .. 350.3 μs)
    variance introduced by outliers: 24% (moderately inflated)
    
    benchmarking evalTestM/interp (has no logs)/mapEven 100
    time                 688.9 ms   (678.3 ms .. 710.5 ms)
                         1.000 R²   (1.000 R² .. 1.000 R²)
    mean                 682.8 ms   (680.4 ms .. 686.6 ms)
    std dev              3.584 ms   (943.0 μs .. 4.414 ms)
    variance introduced by outliers: 19% (moderately inflated)
    
    after
    benchmarking evalTestM/interp (has no logs)/mapEven 1
    time                 1.238 ms   (1.210 ms .. 1.272 ms)
                         0.998 R²   (0.996 R² .. 1.000 R²)
    mean                 1.214 ms   (1.209 ms .. 1.223 ms)
    std dev              22.28 μs   (6.977 μs .. 38.92 μs)
    
    benchmarking evalTestM/interp (has no logs)/mapEven 10
    time                 7.071 ms   (6.908 ms .. 7.271 ms)
                         0.996 R²   (0.993 R² .. 0.999 R²)
    mean                 6.889 ms   (6.840 ms .. 7.002 ms)
    std dev              221.6 μs   (162.1 μs .. 302.6 μs)
    variance introduced by outliers: 13% (moderately inflated)
    
    benchmarking evalTestM/interp (has no logs)/mapEven 100
    time                 984.0 ms   (971.3 ms .. 1.010 s)
                         1.000 R²   (1.000 R² .. 1.000 R²)
    mean                 972.0 ms   (968.3 ms .. 980.5 ms)
    std dev              6.130 ms   (2.378 ms .. 8.410 ms)
    variance introduced by outliers: 19% (moderately inflated)

--- primer/src/Primer/EvalFullInterp.hs
+++ primer/src/Primer/EvalFullInterp.hs
@@ -150,11 +150,26 @@ interp brd tydefs env@(envTm,envTy) dir = \case
   -- TODO: deal with primitives!
   Let _ v e b -> interp brd tydefs (extendTmEnv (Right v) (interp brd tydefs env Syn e) env) dir b
   LetType _ v t b -> interp brd tydefs (extendTyEnv v (interpTy envTy t) env) dir b
+  -- TODO: benchmark two implementations of letrec
+  -- perhaps switch dependant on whether have recursion limit?
+  {-
+  -- lift recursive lets to recusive lets in the metalanguage (haskell)
+  -- this can easily cause deadlocked or infinite-size programs
+  -- and can't be detected
+  -- but has advantage of being lazy and having sharing
   Letrec _ v e t b  -> let e' = interp brd tydefs env' Chk e
                            env' = extendTmEnvWithFVs (Right v) (Ann () e' $ interpTy envTy t)
                                                      (Set.delete (unLocalName v) $ freeVars (Ann () e t))
                                                      env
                        in interp brd tydefs env' dir b
+                       -}
+  -- iteratively unroll the let. this causes reduntant work, but at least we will notice infinite loops
+  Letrec _ v e t b -> let e' n = interp n tydefs (env' $ betaRecursionDepthPred n) Chk e
+                          t' = interpTy envTy t
+                          env' n = extendTmEnvWithFVs (Right v) (Ann () (e' n) t')
+                                                     (Set.delete (unLocalName v) $ freeVars (Ann () e t))
+                                                     env
+                      in interp brd tydefs (env' brd) dir b
   -- In step interpreter, case which does not discriminate is lazy. Same here for consistency
   Case _ _ [] (CaseFallback e) -> interp brd tydefs env Chk e
   Case _ e brs fb -> -- this relies on @e@ computing to normal form lazily
