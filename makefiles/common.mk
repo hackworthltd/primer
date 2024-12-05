c581b89d324070a1bda314afa8384d9d46cab2b4
Drew Hess <src@drewhess.com>
2024-12-05 17:49:28 +0900

    fix: remove `configure` target for makefiles
    Signed-off-by: Drew Hess <src@drewhess.com>

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -3,10 +3,10 @@
 # Most commands assume you're running this from the top-level `nix
 # develop` shell.
 
-build:	configure
+build:
 	cabal build
 
-package-targets = configure test haddock clean
+package-targets = test haddock clean
 
 $(package-targets):
 	cabal $@
