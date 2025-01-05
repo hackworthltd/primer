a28776acc344024f6cad6592a0de9daac062d9fc
George Thomas <georgefsthomas@gmail.com>
2025-01-05 22:42:51 +0000

    refactor(primer-miso): Use a stylesheet
    This should be easier to work with than using inline styles for everything. It is also necessary for some functionality, e.g. changing styles on hover, which relies on a pseudo-selector and thus can't be used inline.
    
    Signed-off-by: George Thomas <georgefsthomas@gmail.com>

--- primer-miso/exe/Main.hs
+++ primer-miso/exe/Main.hs
@@ -16,16 +16,38 @@ main = JSaddle.Wasm.run start
 
 #else
 
+{-# LANGUAGE NoOverloadedStrings #-}
+
 module Main (main) where
 
 import Foreword
 
-import Language.Javascript.JSaddle.Warp ( debug )
+import JSDOM (currentDocument)
+import JSDOM.Generated.Document (createElement, getHead)
+import JSDOM.Generated.HTMLLinkElement (setHref, setRel)
+import JSDOM.Generated.Node (appendChild_)
+import JSDOM.Types (Element (Element), HTMLLinkElement (HTMLLinkElement), MonadDOM, ToJSString, unsafeCastTo)
+import Language.Javascript.JSaddle.Warp (debugOr)
+import Network.Wai.Application.Static (defaultWebAppSettings, staticApp)
 import Primer.Miso (start)
 
 -- Note that `debug` works with `cabal repl` but not `cabal run`.
 -- The best workflow is to run `ghcid -c "cabal repl primer-miso" -W -T ':main'`.
 main :: IO ()
-main = debug 8000 start
+main =
+  debugOr
+    8000
+    (insertStylesheet "style.css" >> start)
+    (staticApp $ defaultWebAppSettings "frontend")
+
+-- https://github.com/ghcjs/jsaddle/pull/149#issuecomment-2525187769
+insertStylesheet :: (MonadDOM m, MonadFail m, ToJSString val) => val -> m ()
+insertStylesheet stylesheetUrl = do
+  Just doc <- currentDocument
+  Just headEl <- getHead doc
+  linkEl <- unsafeCastTo HTMLLinkElement =<< createElement doc "link"
+  setRel linkEl "stylesheet"
+  setHref linkEl stylesheetUrl
+  appendChild_ headEl linkEl
 
 #endif
