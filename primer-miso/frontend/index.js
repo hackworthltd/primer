c00f8e53bafbc84193d31053f0628cf4c6f9fbaa
Drew Hess <src@drewhess.com>
2025-11-04 13:10:40 +0100

    feat: on Wasm projects, set suggested heap size to 64m
    This is a bit of cargo cult engineering as I can't find any specific
    references to why this is a good idea, but people who should know are
    doing this for their GHC Wasm projects, therefore so will we.
    
    Signed-off-by: Drew Hess <src@drewhess.com>

--- primer-miso/frontend/index.js
+++ primer-miso/frontend/index.js
@@ -2,19 +2,19 @@ import { WASI, OpenFile, File, ConsoleStdout } from "https://cdn.jsdelivr.net/np
 import ghc_wasm_jsffi from "./ghc_wasm_jsffi.js";
 
 const args = [];
-const env = [];
+const env = ["GHCRTS=-H64m"];
 const fds = [
-  new OpenFile(new File([])), // stdin
-  ConsoleStdout.lineBuffered((msg) => console.log(`[WASI stdout] ${msg}`)),
-  ConsoleStdout.lineBuffered((msg) => console.warn(`[WASI stderr] ${msg}`)),
+	new OpenFile(new File([])), // stdin
+	ConsoleStdout.lineBuffered((msg) => console.log(`[WASI stdout] ${msg}`)),
+	ConsoleStdout.lineBuffered((msg) => console.warn(`[WASI stderr] ${msg}`)),
 ];
 const options = { debug: false };
 const wasi = new WASI(args, env, fds, options);
 
 const instance_exports = {};
 const { instance } = await WebAssembly.instantiateStreaming(fetch("bin.wasm"), {
-  wasi_snapshot_preview1: wasi.wasiImport,
-  ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
+	wasi_snapshot_preview1: wasi.wasiImport,
+	ghc_wasm_jsffi: ghc_wasm_jsffi(instance_exports),
 });
 Object.assign(instance_exports, instance.exports);
 
