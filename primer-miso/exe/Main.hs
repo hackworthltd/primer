13005e079a6706729b216265c9b940a829bdcc9b
George Thomas <georgefsthomas@gmail.com>
2025-12-27 14:50:01 +0000

    drop jsaddle-warp support

--- primer-miso/exe/Main.hs
+++ primer-miso/exe/Main.hs
@@ -20,38 +20,13 @@ foreign export javascript "hs_start" main :: IO ()
 
 #else
 
-{-# LANGUAGE NoOverloadedStrings #-}
-
 module Main (main) where
 
 import Foreword
 
-import JSDOM (currentDocument)
-import JSDOM.Generated.Document (createElement, getHead)
-import JSDOM.Generated.HTMLLinkElement (setHref, setRel)
-import JSDOM.Generated.Node (appendChild_)
-import JSDOM.Types (Element (Element), HTMLLinkElement (HTMLLinkElement), MonadDOM, ToJSString, unsafeCastTo)
-import Language.Javascript.JSaddle.Warp (debugOr)
-import Network.Wai.Application.Static (defaultWebAppSettings, staticApp)
-import Primer.Miso (start)
-
--- Note that `debug` works with `cabal repl` but not `cabal run`.
--- This is really designed to be used with GHCID or similar.
 main :: IO ()
-main =
-  debugOr
-    8001
-    (insertStylesheet "style.css" >> start)
-    (staticApp $ defaultWebAppSettings "frontend")
-
--- https://github.com/ghcjs/jsaddle/pull/149#issuecomment-2525187769
-insertStylesheet :: (MonadDOM m, MonadFail m, ToJSString val) => val -> m ()
-insertStylesheet stylesheetUrl = do
-  Just doc <- currentDocument
-  Just headEl <- getHead doc
-  linkEl <- unsafeCastTo HTMLLinkElement =<< createElement doc "link"
-  setRel linkEl "stylesheet"
-  setHref linkEl stylesheetUrl
-  appendChild_ headEl linkEl
+main = do
+  putStrLn @Text "Native compilation is only supported for HLS usage. Nothing to run."
+  exitFailure
 
 #endif
